7a3c488652f5b2aeb1610e18dce49a148c69e1b6
Anandarafi <ananda_rafi_27rpl@student.smktelkom-mlg.sch.id>
2020-02-18 09:46:35 +0700

    First Commit

---
+++ app/Http/Controllers/RajaOngkirController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use RajaOngkir;
+use Illuminate\Http\Request;
+
+class RajaOngkirController extends Controller
+{
+   
+    public function awal()
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://api.rajaongkir.com/starter/city",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_HTTPHEADER => array(
+            "Content-Type: application/x-www-form-urlencoded",
+            "key: 0df6d5bf733214af6c6644eb8717c92c"
+        ),
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+        echo "cURL Error #:" . $err;
+        } else {
+            echo $response;
+        }
+    }
+    public function tampil(Request $request)
+    {        
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://api.rajaongkir.com/starter/city",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_HTTPHEADER => array(
+            "Content-Type: application/x-www-form-urlencoded",
+            "key: 0df6d5bf733214af6c6644eb8717c92c"
+        ),
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            $response = json_decode($response);
+            $data = $response->rajaongkir->results;
+            $search = $request->get('j');
+            if($search == ''){
+                return view('RajaOngkir',['Data'=>$data]);
+            }
+            else{
+            return view('RajaOngkirShow',['Data'=>$data, 'Search'=>$search]);
+            }
+        }
+    }
+}
